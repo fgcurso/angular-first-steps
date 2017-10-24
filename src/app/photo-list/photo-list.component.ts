bbe4135b4e40dee9074471c30edf816c635663c0
Facundo <fgoyena@gmail.com>
2017-10-23 21:04:16 -0300

    Implementacion simple de preview en componente (sin directiva).

--- src/app/photo-list/photo-list.component.ts
+++ src/app/photo-list/photo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { PhotoService } from '../photo.service';
 
 @Component({
@@ -8,14 +8,35 @@ import { PhotoService } from '../photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
+  @ViewChild('previewElement')
+  private previewEl : ElementRef;
+
   photos : Array<any>;
+
+  selectedPhoto? : any;
 
   constructor(private service : PhotoService) { }
 
-  ngOnInit() {
+  ngOnInit() : void {
     this.service.getAll().subscribe((photos) => {
       this.photos = photos.json();
     });
+  }
+
+  onMouseEnter(e : MouseEvent, photo) : void {
+    if (!this.previewEl) {
+      return;
+    }
+
+    this.selectedPhoto = photo;
+    this.previewEl.nativeElement.style.top =
+      `${e.clientY}px`;
+    this.previewEl.nativeElement.style.left =
+      `${e.clientX + 15}px`;
+  }
+
+  onMouseLeave() : void {
+    this.selectedPhoto = null;
   }
 
 }
